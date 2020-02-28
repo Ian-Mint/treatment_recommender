9c32ce9af1fbcbcf3fb306556f9eab3cbf8a7601
Ian Pegg <ianpegg9@gmail.com>
2020-02-28 10:14:42 -0800

    data_loader in early working state

--- data_loader.py
+++ data_loader.py
@@ -2,13 +2,13 @@ import pickle
 import pandas as pd
 import numpy as np
 from typing import Union
-# from keras.preprocessing import sequence
+from keras.preprocessing.sequence import pad_sequences
 
 import config
 
 
 class Data:
-    def load(self):
+    def __init__(self):
         # load static features
         self.demographics = load_demographics(config.demographics_path)
         self.elixhauser = load_pickle(config.elixhauser_path)
@@ -16,23 +16,16 @@ class Data:
         # load time-series features and labels
         self.fluids = load_pickle(config.fluids_path)
         self.vasopressin = load_pickle(config.vasopressin_path)
-        self.time_series = load_pickle(config.main_data_path)
+        time_series = load_pickle(config.main_data_path)
 
         # Reduce elixhauser and demographics to include keys in time-series data
         self.demographics = {k: v for k, v in self.demographics.items() if k in self.fluids.keys()}
         self.elixhauser = {k: v for k, v in self.elixhauser.items() if k in self.fluids.keys()}
 
-    def combine(self):
-        np.array()
-
-    def to_sequence(self):
-        pass
-
-    def encode(self, data: Union[pd.Series, pd.DataFrame, np.ndarray], encoding: dict) \
-            -> Union[pd.Series, pd.DataFrame, np.ndarray]:
-        for k, v in encoding.items():
-            data[data == k] = v
-        return data
+        self.hadm_ids = tuple(time_series.keys())
+        self.maxlen = max((x.shape[0] for x in time_series.values()))
+        self.time_series = pad_sequences(time_series.values(), maxlen=self.maxlen, dtype='float', value=np.nan)
+        # TODO: nan might need to be replaced -> self.time_series[self.time_series.isnan()] = -1
 
 
 def load_demographics(path: str):
@@ -44,7 +37,3 @@ def load_demographics(path: str):
 def load_pickle(path: str) -> dict:
     with open(path, 'rb') as f:
         return pickle.load(f)
-
-
-d = Data()
-d.load()
