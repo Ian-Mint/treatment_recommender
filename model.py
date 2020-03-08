9dcfcc2df0ed5d47586b824b3eb805e686efc3cd
Ian Pegg <ianpegg9@gmail.com>
2020-03-08 00:32:16 -0800

    removed gpu choices that tensorflow takes care of

--- model.py
+++ model.py
@@ -2,10 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import LSTM, Dense, Masking, TimeDistributed
 
-use_gpu = tf.test.is_built_with_gpu_support() and tf.test.is_gpu_available(cuda_only=True)
-if use_gpu:
-    from tensorflow.keras.layers import CuDNNLSTM
-
 
 def build_model(width, batch_size, n_timesteps, n_features, n_layers=1,
                 dropout=0, recurrent_dropout=0, ) -> keras.Model:
@@ -31,13 +27,8 @@ def build_model(width, batch_size, n_timesteps, n_features, n_layers=1,
     model.add(Masking(mask_value=-1, batch_input_shape=[batch_size, n_timesteps, n_features]))
 
     for i in range(n_layers):
-        if use_gpu:
-            lstm_kwargs.pop('dropout')
-            lstm_kwargs.pop('recurrent_dropout')
-            lstm = CuDNNLSTM(width, **lstm_kwargs)
-        else:
-            lstm = LSTM(width, **lstm_kwargs)
-        model.add(lstm)
+        model.add(LSTM(width, **lstm_kwargs))
+
     model.add(TimeDistributed(Dense(1)))
     model.compile(optimizer='rmsprop', loss='mse')
 
