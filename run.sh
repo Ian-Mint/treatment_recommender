4d4444a4c63ee35ec223a605ee52a024e44d5f90
Ian Pegg <ianpegg9@gmail.com>
2020-03-13 12:52:41 -0700

    new models

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-for j in 4 8 32
+for j in 4 8 16 32
 do
-    for i in 4 8 16
-    do
-	python train.py --epochs 100 --batch_size 50 --layers 2 --lookback $i --width $j
-    done
+    python train.py --epochs 100 --batch_size 50 --layers 1 --lookback 4 --width $j
 done
 
 paplay /usr/share/sounds/gnome/default/alerts/glass.ogg 
